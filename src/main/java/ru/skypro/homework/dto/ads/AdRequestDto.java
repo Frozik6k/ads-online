7fc8f3d93076d4a05065e9ac275114bcc41ab62d
Frozi <Frozik6k@yandex.ru>
2025-09-08 14:14:54 +0500

    price заменил тип на BigDicimal

--- src/main/java/ru/skypro/homework/dto/ads/AdRequestDto.java
+++ src/main/java/ru/skypro/homework/dto/ads/AdRequestDto.java
@@ -3,12 +3,14 @@ package ru.skypro.homework.dto.ads;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class AdRequestDto {
     private String title;
     
-    private int price;
+    private BigDecimal price;
 
     private String description;
 }
