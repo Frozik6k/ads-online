fc504bc458cfc34603285f4ac1eb9bb91232fe7d
lNasralnikl <raidar920357@gmail.com>
2025-09-08 19:21:28 +0500

    Внес коррективы по ревью

--- src/main/java/ru/skypro/homework/controller/user/UserController.java
+++ src/main/java/ru/skypro/homework/controller/user/UserController.java
@@ -2,38 +2,33 @@ package ru.skypro.homework.controller.user;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import ru.skypro.homework.dto.user.NewPassword;
-import ru.skypro.homework.dto.user.UpdateUser;
-import ru.skypro.homework.dto.user.User;
+import ru.skypro.homework.dto.user.NewPasswordRequest;
+import ru.skypro.homework.dto.user.UpdateUserDto;
+import ru.skypro.homework.dto.user.UserDto;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @PostMapping("/set_password")
-    public ResponseEntity<Void> setPassword(@RequestBody NewPassword passwordData){
-
+    public ResponseEntity<Void> setPassword(@RequestBody NewPasswordRequest passwordData){
         return ResponseEntity.ok().build();
-
     }
 
     @GetMapping("/me")
-    public ResponseEntity<User> getUser(){
-
-        User userData = new User();
-        return ResponseEntity.ok(userData);
-
+    public UserDto getUser(){
+        UserDto userData = new UserDto();
+        return userData;
     }
 
     @PatchMapping("/me")
-    public ResponseEntity<UpdateUser> updateUser(UpdateUser updateUser){
+    public UpdateUserDto updateUser(@RequestBody UpdateUserDto updateUser){
 
-        UpdateUser updatedUser = new UpdateUser();
+        UpdateUserDto updatedUser = new UpdateUserDto();
 
-        return ResponseEntity.ok(updatedUser);
+        return updatedUser;
     }
 
     @PatchMapping(value = "/me/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
