b0020eca2adf892ea8beabe609aa6e715d84ad18
Frozi <Frozik6k@yandex.ru>
2025-09-09 01:07:19 +0500

    Исправил не правильный импорт валидаторов

--- src/main/java/ru/skypro/homework/dto/user/NewPasswordRequest.java
+++ src/main/java/ru/skypro/homework/dto/user/NewPasswordRequest.java
@@ -1,10 +1,9 @@
 package ru.skypro.homework.dto.user;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
