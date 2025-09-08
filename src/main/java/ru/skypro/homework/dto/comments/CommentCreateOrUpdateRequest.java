af528b594878782d03efbbeb3db0f5d856b7b057
Frozi <Frozik6k@yandex.ru>
2025-09-08 20:11:57 +0500

    Revert "Merge pull request #4 from Frozik6k/comments"
    This reverts commit e8f40c94cf0bc9f251a7b6a61420d37cd114001e, reversing
    changes made to f6bdf4270462f39d09542029dcce981283e3a8aa.
    
    i
    
    :wq

--- src/main/java/ru/skypro/homework/dto/comments/CommentCreateOrUpdateRequest.java
+++ src/main/java/ru/skypro/homework/dto/comments/CommentCreateOrUpdateRequest.java
@@ -1,14 +1,15 @@
 package ru.skypro.homework.dto.comments;
 
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class CommentCreateOrUpdateRequest {
+public class CreateOrUpdateCommentRequest {
     @Size(min = 8, max = 64, message = "Текст комментария")
     private String text;
 }
